7d324bf7a4af81dd6a61f4adfd8f7154ece204a3
nikivavlt <nikivavlt@gmail.com>
2026-08-01 17:44:49 +0300

    Add variable, constant, conditional statements and help command

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+// const appName string = "Notes"
+const appName = "Notes"
+
 func main() {
-	fmt.Println("Welcome to Notes app")
+	// var command string
+	command := ""
+
+	// fmt.Println("Welcome to", appName+" app!")
+	fmt.Printf("Welcome to %s app!\n", appName)
+	fmt.Println(`Type "help" to see available commands.`)
+	fmt.Print("> ")
+
+	fmt.Scan(&command)
+
+	if command == "help" {
+		fmt.Println("\nAvailable commands:")
+		fmt.Println("	help")
+	} else {
+		fmt.Println(`Unknown command. Type "help".`)
+	}
 }
