5f93bb288515299433f94acc30d5bc020fa81249
nikivavlt <nikivavlt@gmail.com>
2026-08-02 17:18:06 +0300

    Add slice and list/add commands, parse user input

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // const appName string = "Notes"
 const appName = "Notes"
 
 func main() {
+	// first explain array and why not?
+	notes := []string{}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	// fmt.Println("Welcome to", appName+" app!")
@@ -21,16 +25,30 @@ func main() {
 
 		// fmt.Scan(&command)
 		scanner.Scan()
-		command := scanner.Text()
+		userInput := scanner.Text()
+		// why not .Split() ?
+		inputParts := strings.Fields(userInput)
 
-		switch command {
+		switch inputParts[0] {
 		case "":
 			continue
 
 		case "help":
 			fmt.Println("Available commands:")
 			fmt.Println("   help")
+			fmt.Println("   list")
+			fmt.Println("   add")
 			fmt.Println("   exit")
+
+		case "add":
+			notes = append(notes, strings.Join(inputParts[1:], " "))
+
+		case "list":
+			// fmt.Println(notes)
+			fmt.Println("List of notes:")
+			for index, note := range notes {
+				fmt.Printf("%d. %s\n", index, note)
+			}
 
 		case "exit":
 			fmt.Println("Goodbye!")
