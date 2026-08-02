87da586795b6cdff1f8f333f79652413fe900aa0
nikivavlt <nikivavlt@gmail.com>
2026-08-02 14:08:51 +0300

    Add switch, scanner, infinity loop and handle empty input case

--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 // const appName string = "Notes"
 const appName = "Notes"
 
 func main() {
-	// var command string
-	command := ""
+	scanner := bufio.NewScanner(os.Stdin)
 
 	// fmt.Println("Welcome to", appName+" app!")
 	fmt.Printf("Welcome to %s app!\n", appName)
 	fmt.Println(`Type "help" to see available commands.`)
-	fmt.Print("> ")
 
-	fmt.Scan(&command)
+	for {
+		fmt.Print("> ")
 
-	if command == "help" {
-		fmt.Println("\nAvailable commands:")
-		fmt.Println("	help")
-	} else {
-		fmt.Println(`Unknown command. Type "help".`)
+		// fmt.Scan(&command)
+		scanner.Scan()
+		command := scanner.Text()
+
+		switch command {
+		case "":
+			continue
+
+		case "help":
+			fmt.Println("Available commands:")
+			fmt.Println("   help")
+			fmt.Println("   exit")
+
+		case "exit":
+			fmt.Println("Goodbye!")
+			// break - instead since it ends loop, but not fn
+			return
+
+		default:
+			fmt.Println(`Unknown command. Type "help".`)
+		}
 	}
 }
