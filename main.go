b0b23e0306984b41fc40ccaebf42fc30bb51be89
nikivavlt <nikivavlt@gmail.com>
2026-08-06 13:00:30 +0300

    Add note struct, method, show command, helper find fn

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 // const appName string = "Notes"
 const appName = "Notes"
 
+type Note struct {
+	ID   int
+	Text string
+}
+
+// A method is a function associated with a type, value receiver /pointer receiver
+func (n Note) Print() {
+	fmt.Printf("%d. %s\n", n.ID, n.Text)
+}
+
 func main() {
 	// first explain array and why not? arrays length is part of its type
-	notes := []string{}
+	notes := []Note{}
+	globalID := 1
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -29,6 +41,7 @@ func main() {
 			break
 		}
 
+		// simplify this
 		userInput := scanner.Text()
 		// why not .Split() ?
 		inputParts := strings.Fields(userInput)
@@ -36,7 +49,7 @@ func main() {
 		if len(inputParts) == 0 {
 			continue
 		}
-		
+
 		// explain why panic possible here
 		switch inputParts[0] {
 		// case "":
@@ -46,7 +59,8 @@ func main() {
 			fmt.Println("Available commands:")
 			fmt.Println("   help")
 			fmt.Println("   list")
-			fmt.Println("   add")
+			fmt.Println("   add <text>")
+			fmt.Println("   show <id>")
 			fmt.Println("   exit")
 
 		case "add":
@@ -54,7 +68,15 @@ func main() {
 			if len(inputParts) < 2 {
 				fmt.Println("Usage: add <text>")
 			} else {
-				notes = append(notes, strings.Join(inputParts[1:], " "))
+				notes = append(
+					notes,
+					Note{
+						ID:   globalID,
+						Text: strings.Join(inputParts[1:], " "),
+					})
+
+				globalID += 1
+
 				fmt.Println("Note added successfully.")
 			}
 
@@ -66,9 +88,24 @@ func main() {
 
 			// fmt.Println(notes)
 			fmt.Println("List of notes:")
-			for index, note := range notes {
-				fmt.Printf("%d. %s\n", index+1, note)
+			for _, note := range notes {
+				note.Print()
 			}
+
+		case "show":
+			id, err := strconv.Atoi(inputParts[1])
+			if err != nil {
+				fmt.Println("Invalid note ID.")
+				continue
+			}
+
+			note, found := findNote(id, notes)
+			if !found {
+				fmt.Println("Note not found.")
+				return
+			}
+
+			note.Print()
 
 		case "exit":
 			fmt.Println("Goodbye!")
@@ -79,4 +116,14 @@ func main() {
 			fmt.Println(`Unknown command. Type "help".`)
 		}
 	}
+}
+
+func findNote(id int, notes []Note) (Note, bool){
+	for _, note := range notes {
+		if note.ID == id {
+			return note, true
+		}
+	}
+
+	return Note{}, false
 }
